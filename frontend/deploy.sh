0dc39c6548a65dbf81ad35588026805072fd292b
Il jo Yoo <iljo.joseph.yoo@gmail.com>
2022-08-28 11:44:25 +0900

    CICD Enabled

---
+++ frontend/deploy.sh
@@ -0,0 +1,17 @@
+echo "Kill all PM2 pids"
+sudo pm2 kill
+cd /var/services/homes/solduma/my-page
+
+echo "Update app"
+git fetch --all
+git reset --hard origin/main
+cd /var/services/homes/solduma/my-page/frontend
+
+echo "Install dependencies"
+sudo npm i
+
+echo "Build app"
+sudo npm run build
+
+echo "Run app"
+sudo pm2 run --name frontend npm -- start
