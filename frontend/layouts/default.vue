bc22cf71b48d660e11515fd2a9c453afabcd4477
Il jo Yoo <iljo.joseph.yoo@gmail.com>
2022-09-07 13:18:47 +0900

    Korean version introduction added

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NavBar />
+    <div id="wrapper">
+      <main>
+        <h2 class="hide">This is a main area</h2>
+        <div class="viewWrapper">
+          <Nuxt />
+        </div>
+      </main>
+    </div>
+    <Footer />
+  </div>
+</template>
